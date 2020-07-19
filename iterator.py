d7aabb42a839ac1ea75a94a94fffd4698ef99780
Seung Hoon Lee <shoonhye@gmail.com>
2020-07-19 11:12:48 -0700

    new_file

---
+++ iterator.py
@@ -0,0 +1,13 @@
+
+
+a = [1,2,3]
+b = iter(a)
+print(next(b)) # 1
+print(next(b)) # 2
+print(next(b)) # 3
+
+# print(next(b)) # ERROR since we run out of element
+
+# Do this instead
+print(next(b, None))
+
